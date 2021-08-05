99494c93b7f0c059f67e93c35e5475eca317bf0e
tagplus5 <tagplus5@gmail.com>
2021-08-05 11:38:47 +0300

    initial commit

---
+++ components/DateRangePickerExporter.jsx
@@ -0,0 +1,13 @@
+import { StylesProvider } from '@material-ui/core/styles';
+import DateRangePickerWrapper from './DateRangePickerWrapper';
+import generateClassName from '../generateClassName';
+
+const DateRangePickerExporter = (props) => (
+    <StylesProvider generateClassName={generateClassName}>
+        <DateRangePickerWrapper
+            {...props}
+        />
+    </StylesProvider>
+);
+
+export default DateRangePickerExporter;
